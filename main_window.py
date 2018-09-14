bcfeb5143069826478101c0d4c7cecf70c23b7b1
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2018-09-14 15:52:52 +0200

    improved db structure

--- main_window.py
+++ main_window.py
@@ -1,4 +1,6 @@
 import sys
+
+import math
 from PyQt5 import QtSql
 from PyQt5.QtCore import QVariant, pyqtSignal
 from PyQt5.QtSql import QSqlQuery
@@ -17,6 +19,7 @@ class App(QMainWindow):
         self.top = 10
         self.width = 1280
         self.height = 800
+        self.db = None
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowTitle(self.title)
         self.setWindowIcon(QIcon('favicon.ico'))
@@ -33,6 +36,7 @@ class App(QMainWindow):
         self.setCentralWidget(self.homeWidget)
 
         self.homeWidget.tournament_opened.connect(self.open_tournament)
+        self.homeWidget.tournament_created.connect(self.create_tournament)
 
         self.show()
 
@@ -49,24 +53,125 @@ class App(QMainWindow):
         self.centralWidget().setParent(None)  # prevent_deletion
         self.setCentralWidget(self.tournamentWidget)
 
+    def create_tournament(self, data):
+        print(data)
+        self.save_tournament_to_database(data)
+
     # todo: move this from here into model part of the code
-    @staticmethod
-    def connect_to_database():
-        db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('flunkyrock.db')
-        db.open()
+
+    def save_tournament_to_database(self, data):
+
+        def get_current_id(table):
+            query = QtSql.QSqlQuery()
+            query.prepare('SELECT * FROM SQLITE_SEQUENCE WHERE name=:table')
+            query.bindValue(':table', table)
+            if query.exec_():
+                seq = query.record().indexOf("seq")
+                query.next()
+                return query.record().value(seq)
+            else:
+                print("ERROR: request failed: ", query.lastError().text())
+                return -2
+
+        if self.db is None:
+            self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+            self.db.setDatabaseName('flunkyrock.db')
+        self.db.open()
+        query = QtSql.QSqlQuery()
+
+        if 'Tournaments' not in self.db.tables():
+            self.db.close()
+            print('db_error: table Tournaments not found')
+            return
+
+        print('trying to add %s to db...' % data['name'])
+        try:
+            self.db.transaction()
+            query.prepare('INSERT INTO Tournaments(name) VALUES (:name)')
+            query.bindValue(':name', data['name'])
+            query.exec_()
+            tournament_id = get_current_id('Tournaments')
+            # create group_stage
+            query.prepare('INSERT INTO Tournament_Stages(tournament, stage_index) VALUES (:t_id, 1)')
+            query.bindValue(':t_id', tournament_id)
+            query.exec_()
+            ts_id = get_current_id('Tournament_Stages')
+            query.prepare('INSERT INTO Group_Stages(tournament_stage) VALUES (:ts_id)')
+            query.bindValue(':ts_id', ts_id)
+            query.exec_()
+            query.prepare('INSERT INTO Groups(group_stage, size) VALUES(:gs_id, :size)')
+            for g in range(0, int(math.ceil(len(data['teams'])/int(data['group_size'])))):
+                query.bindValue(':gs_id', ts_id)
+                query.bindValue(':size', data['group_size'])
+                query.exec_()
+
+            # create ko-stages
+            teams_in_ko = data['teams_in_ko']
+            index = 2
+            while teams_in_ko > 1:
+                query.prepare('INSERT INTO Tournament_Stages(tournament, stage_index) VALUES (:t_id, :idx)')
+                query.bindValue(':t_id', tournament_id)
+                query.bindValue(':idx', index)
+                query.exec_()
+                ts_id = get_current_id('Tournament_Stages')
+                query.prepare('INSERT INTO KO_Stages(tournament_stage) VALUES (:ts_id)')
+                query.bindValue(':ts_id', ts_id)
+                query.exec_()
+                index += 1
+                teams_in_ko /= 2
+
+            # add teams
+            query.exec_('SELECT * FROM Teams')
+            db_teams = {}
+            while query.next():
+                db_teams[query.value('name')] = query.value('id')
+            for team in data['teams']:
+                if team != '':
+                    if team in db_teams:
+                        team_id = db_teams[team]
+                    else:
+                        query.prepare('INSERT INTO Teams(name) VALUES(:team)')
+                        query.bindValue(':team', team)
+                        query.exec_()
+                        team_id = get_current_id('Teams')
+                    query.prepare('INSERT INTO Tournament_Teams(tournament, team) VALUES(:tour_id, :team_id)')
+                    query.bindValue(':tour_id', tournament_id)
+                    query.bindValue(':team_id', team_id)
+                    query.exec_()
+
+            self.db.commit()
+
+            print('added %s to db...' % data['name'])
+            print('db close')
+            self.db.close()
+            self.data = self.connect_to_database()
+            if type(self.centralWidget()) == HomeWidget:
+                self.homeWidget = HomeWidget(self.data)
+                self.homeWidget.tournament_opened.connect(self.open_tournament)
+                self.homeWidget.tournament_created.connect(self.create_tournament)
+                self.setCentralWidget(self.homeWidget)
+        except IndexError:
+            print('db_error:', query.lastError(), query.lastError().text())
+            self.db.rollback()
+            print('db close')
+            self.db.close()
+
+    def connect_to_database(self):
+        if self.db is None:
+            self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+            self.db.setDatabaseName('flunkyrock.db')
+        self.db.open()
         query = QtSql.QSqlQuery()
 
         # this is just for testing purposes, of course there won't be a full copy of the database as a python dict
         # in the finished version, but it is very convenient for now
         def read_data():
             data = {}
-            for table in db.tables():
+            for table in self.db.tables():
 
                 data[table] = []
-                keys = [db.record(table).field(x).name() for x in range(db.record(table).count())]
+                keys = [self.db.record(table).field(x).name() for x in range(self.db.record(table).count())]
                 query.exec_('SELECT * from %s' % table)
-
                 while query.next():
                     entry = {}
                     for key in keys:
@@ -74,18 +179,72 @@ class App(QMainWindow):
                     data[table].append(entry)
             return data
 
-        if 'teams' not in db.tables():
-            # create tables
-            query.exec_('CREATE TABLE teams('
-                        'id INTEGER PRIMARY KEY, '
-                        'name VARCHAR(40) NOT NULL)')
-            query.exec_('CREATE TABLE tournaments('
-                        'id INTEGER PRIMARY KEY, '
-                        'name VARCHAR(40) NOT NULL, '
-                        'stylesheet VARCHAR(50) NOT NULL )')
+        if 'Teams' not in self.db.tables():
+
+            query.exec_('CREATE TABLE Teams ('
+                        'id INTEGER PRIMARY KEY AUTOINCREMENT ,'
+                        'name VARCHAR(40) NOT NULL'
+                        ')')
+
+            query.exec_('CREATE TABLE Tournaments ('
+                        'id INTEGER PRIMARY KEY AUTOINCREMENT ,'
+                        'name VARCHAR(40) NOT NULL,'
+                        'stylesheet VARCHAR(50)'
+                        ')')
+
+            query.exec_('CREATE TABLE Tournament_Teams ('
+                        'tournament INTEGER,'
+                        'team INTEGER,'
+                        'FOREIGN KEY (tournament) REFERENCES Tournaments(id),'
+                        'FOREIGN KEY (team) REFERENCES Teams(id)'
+                        ')')
+            
+            query.exec_('CREATE TABLE Tournament_Stages ('
+                        'id INTEGER PRIMARY KEY AUTOINCREMENT ,'
+                        'tournament INTEGER,'
+                        'stage_index INTEGER,'
+                        'FOREIGN KEY(tournament) REFERENCES Tournaments(id)'
+                        ')')
+
+            query.exec_('CREATE TABLE Group_Stages ('
+                        'tournament_stage INTEGER,'
+                        'FOREIGN KEY(tournament_stage) REFERENCES Tournament_Stages(id)'
+                        ')')
+
+            query.exec_('CREATE TABLE KO_Stages ('
+                        'tournament_stage INTEGER,'
+                        'FOREIGN KEY(tournament_stage) REFERENCES Tournament_Stages(id)'
+                        ')')
+
+            query.exec_('CREATE TABLE Groups ('
+                        'id INTEGER PRIMARY KEY AUTOINCREMENT ,'
+                        'group_stage INTEGER,'
+                        'size INTEGER,'
+                        'FOREIGN KEY (group_stage) REFERENCES Group_Stages(tournament_stage)'
+                        ')')
+
+            query.exec_('CREATE TABLE Group_Teams ('
+                        'group_id INTEGER,'
+                        'team INTEGER,'
+                        'FOREIGN KEY (group_id) REFERENCES Groups(id),'
+                        'FOREIGN KEY (team) REFERENCES Teams(id)'
+                        ')')
+
+            query.exec_('CREATE TABLE Matches ('
+                        'id INTEGER PRIMARY KEY AUTOINCREMENT ,'
+                        'team1 INTEGER,'
+                        'team2 INTEGER,'
+                        'team1_score INTEGER,'
+                        'team2_score INTEGER,'
+                        'status INTEGER,'
+                        'tournament_stage INTEGER,'
+                        'FOREIGN KEY(team1) REFERENCES Teams(id),'
+                        'FOREIGN KEY(team2) REFERENCES Teams(id),'
+                        'FOREIGN KEY(tournament_stage) REFERENCES Tournament_Stages(id)'
+                        ')')
 
             # insert dummy tournaments, todo: remove, obviously
-            query.prepare('INSERT INTO tournaments(name, stylesheet) VALUES (:tournament_name, :style_sheet)')
+            query.prepare('INSERT INTO Tournaments(name, stylesheet) VALUES (:tournament_name, :style_sheet)')
             tournaments = [
                             ('FlunkyRock 2015', 'background-color: rgb(30,143,158); color: rgb(0,0,80)'),
                             ('FlunkyRock 2016', 'background-color: rgb(70,190,130); color: rgb(0,80,0)'),
@@ -95,24 +254,24 @@ class App(QMainWindow):
             query.bindValue(':style_sheet', [QVariant(t[1]) for t in tournaments])
             if not query.execBatch(mode=QSqlQuery.ValuesAsRows):
                 print(query.lastError().text())
-                db.close()
-                return {'tournaments': [], 'teams': []}
+                self.db.close()
+                return {'Tournaments': [], 'Teams': []}
 
             # insert dummy teams, todo: remove, obviously
-            query.prepare('INSERT INTO teams(name) VALUES (:team_name)')
+            query.prepare('INSERT INTO Teams(name) VALUES (:team_name)')
             teams = ['Flunkengötter', 'Beardy Beer', 'Sportfreunde Gartenhaus', 'Ralles Raketen',
                      'Die Bierprinzessinnen']
             query.bindValue(':team_name', [QVariant(t) for t in teams])
             if not query.execBatch(mode=QSqlQuery.ValuesAsRows):
                 print(query.lastError().text())
-                db.close()
-                return {'tournaments': [], 'teams': []}
+                self.db.close()
+                return {'Tournaments': [], 'Teams': []}
             else:
                 print('created tables')
         else:
             print('found tables')
         data = read_data()
-        db.close()
+        self.db.close()
         return data
 
 
@@ -133,6 +292,7 @@ class TournamentWidget(QWidget):
 
 class HomeWidget(QWidget):
     tournament_opened = pyqtSignal(dict)
+    tournament_created = pyqtSignal(dict)
 
     def __init__(self, data):
         super().__init__()
@@ -154,7 +314,7 @@ class HomeWidget(QWidget):
         create_tournament_button.clicked.connect(self.show_tournament_wizard)
         r = 0
         c = 0
-        for t in self.data['tournaments']:
+        for t in self.data['Tournaments']:
             bt = QPushButton(t['name'])
             bt.setStyleSheet(t['stylesheet'])
             bt.setIcon(QIcon('favicon.ico'))
@@ -175,7 +335,8 @@ class HomeWidget(QWidget):
 
     def show_tournament_wizard(self):
         if not self.tournament_wizard:
-            self.tournament_wizard = TournamentWizard(self.data['teams'])
+            self.tournament_wizard = TournamentWizard(self.data['Teams'])
+            self.tournament_wizard.tournament_created.connect(self.tournament_created)
 
         def cleanup():
             self.tournament_wizard = None
--- wizards.py
+++ wizards.py
@@ -1,35 +1,52 @@
-from PyQt5.QtCore import QSortFilterProxyModel, Qt
+import math
+from PyQt5.QtCore import QSortFilterProxyModel, Qt, pyqtSignal
+from PyQt5.QtGui import QIcon, QPainter, QPixmap, QColor, QImage
 from PyQt5.QtWidgets import QWizard, QWizardPage, QComboBox, QLabel, QLineEdit, QSpinBox, QFormLayout, \
     QStyle, QListWidgetItem, QListWidget, \
     QCompleter, QSizePolicy, QGridLayout
 
 
 class TournamentWizard(QWizard):
+
+    tournament_created = pyqtSignal(dict)
+
     def __init__(self, teams):
         super().__init__()
         self.setWindowTitle('Create Tournament')
         self.setWindowIcon(self.style().standardIcon(getattr(QStyle, 'SP_FileDialogNewFolder')))
         self.setStyleSheet('background-color: rgb(51,124,99); font-family: Helvetica; font-weight: bold; '
                            'color: rgb(255,255,255)')
+        self.resize(700, 700)
         self.namePage = NamePage([t['name'] for t in teams], parent=self)
         self.tournamentSettingsPage = TournamentSettingsPage(self)
         self.addPage(self.namePage)
         self.addPage(self.tournamentSettingsPage)
-        self.accepted.connect(self.print_current)
+        self.accepted.connect(self.create_tournament)
 
-    def print_current(self):
-        print([self.namePage.teamTable.itemWidget(self.namePage.teamTable.item(i)).currentText()
-               for i in range(0, self.namePage.teamTable.count())])
+        self.namePage.team_num_changed_signal.connect(self.tournamentSettingsPage.num_teams_changed)
+        self.tournamentSettingsPage.num_teams_changed(self.namePage.spinBox.value())
+
+    def create_tournament(self):
+        data = {
+                'name': self.namePage.lineEdit.text(),
+                'teams': [self.namePage.teamTable.itemWidget(self.namePage.teamTable.item(i)).currentText()
+                          for i in range(0, self.namePage.teamTable.count())],
+                'group_size': int(self.tournamentSettingsPage.groupComboBox.currentData()),
+                'teams_in_ko': int(self.tournamentSettingsPage.finalComboBox.currentData())
+                }
+        self.tournament_created.emit(data)
 
 
 class NamePage(QWizardPage):
+    team_num_changed_signal = pyqtSignal(int)
+
     def __init__(self, teams, parent=None):
         super(NamePage, self).__init__(parent)
         self.lineEdit = QLineEdit(self)
         self.lineEdit.setText('FlunkyRock 2018')
         self.spinBox = QSpinBox(self)
         self.spinBox.setMinimum(4)
-        self.spinBox.setValue(12)
+        self.spinBox.setValue(16)
         self.spinBox.valueChanged.connect(self.num_teams_changed)
 
         self.teamTable = QListWidget(self)
@@ -54,12 +71,20 @@ class NamePage(QWizardPage):
             item = QListWidgetItem(self.teamTable)
             team_item = FilteringComboBox(self)
             team_item.currentTextChanged.connect(self.team_selected)
+            px = QPixmap(16, 16)
+            px.fill(QColor(0, 0, 0, 0))
+            painter = QPainter()
+            painter.begin(px)
+            painter.drawText(0, 0, 16, 16, Qt.AlignRight | Qt.AlignVCenter, str(self.teamTable.count()))
+            painter.end()
+            item.setIcon(QIcon(px))
             self.teamTable.addItem(item)
             self.teamTable.setItemWidget(item, team_item)
             item.setSizeHint(team_item.sizeHint())
         while self.spinBox.value() < self.teamTable.count():
             self.teamTable.takeItem(self.teamTable.count()-1)
         self.team_selected()
+        self.team_num_changed_signal.emit(self.spinBox.value())
 
     def team_selected(self):
         selections = {}
@@ -85,16 +110,79 @@ class NamePage(QWizardPage):
 
 
 class TournamentSettingsPage(QWizardPage):
+    final_names = {
+        2: 'Final',
+        4: 'Semi-Final',
+        8: 'Quarter-Final',
+        16: 'Round of 16',
+        32: 'Round of 32',
+        64: 'Round of 64'
+    }
+
     def __init__(self, parent=None):
         super(TournamentSettingsPage, self).__init__(parent)
+        self.num_teams = 0
         self.setLayout(QFormLayout())
-        self.groupSpinBox = QSpinBox()
+        self.groupComboBox = QComboBox()
         self.finalComboBox = QComboBox()
-        self.estimatedGamesLabel = QLabel()
-        self.layout().addRow(QLabel('Number of Groups:'), self.groupSpinBox)
-        self.layout().addRow(QLabel('First KO-Round:'), self.finalComboBox)
-        self.layout().addRow(QLabel('Estimated # of Games:'), self.estimatedGamesLabel)
+        self.maxGamesLabel = QLabel()
+        self.layout().addRow(QLabel('Group size:'), self.groupComboBox)
+        self.layout().addRow(QLabel('Teams in KO Round:'), self.finalComboBox)
+        self.layout().addRow(QLabel('Max # of Games:'), self.maxGamesLabel)
+        self.groupComboBox.currentTextChanged.connect(self.estimate_games)
+        self.finalComboBox.currentTextChanged.connect(self.estimate_games)
 
+    def num_teams_changed(self, num_teams):
+        self.num_teams = num_teams
+
+        self.finalComboBox.clear()
+        for i in range(2, self.num_teams+1):
+            if (i & (i - 1)) == 0:
+                self.finalComboBox.addItem('%r (%s)' % (i, self.final_names[i]), i)
+
+        self.groupComboBox.clear()
+        choices = self.compute_group_size_scores()
+        for choice in choices:
+            self.groupComboBox.addItem('%r (%r Groups, %r open Slots)' %
+                                       (choice[1]['size'], choice[1]['groups'], choice[1]['open_slots']),
+                                       choice[1]['size'])
+            if choice[0] == 0:
+                self.groupComboBox.setItemData(self.groupComboBox.count()-1, QColor.green, Qt.BackgroundRole)
+
+    def compute_group_size_scores(self):
+        choices = []
+        for group_size in range(3, self.num_teams+1).__reversed__():
+
+            rest = self.num_teams % group_size
+            num_groups = float(self.num_teams) / float(group_size)
+            prev_g = float(self.num_teams) / float(group_size - 1)
+            groups = math.ceil(num_groups)
+            open_slots = (groups * group_size) - self.num_teams
+
+            score = - open_slots
+            if groups > 1:
+                score -= (groups % 2) * 2
+
+            if group_size < 4:
+                score -= 4 - group_size
+            elif group_size > 5:
+                score -= group_size - 5
+
+            if num_groups < 1 or \
+                    (0 < rest < (group_size / 2.0)) or groups == math.ceil(prev_g):
+                pass
+            else:
+                choices.append((score, {'size': group_size, 'groups': groups, 'open_slots': open_slots}))
+                # print('    %s   |  %s   | %s   |  %s' % (groups, group_size,
+                #                                         open_slots, score))
+        choices = sorted(choices, key=lambda x: x[0])
+        choices.reverse()
+        return choices
+
+    def estimate_games(self):
+        if self.finalComboBox.currentData() is not None and self.groupComboBox.currentData() is not None:
+            est_games = int(math.sqrt(int(self.finalComboBox.currentData())))+int(self.groupComboBox.currentData())-1
+            self.maxGamesLabel.setText(str(est_games))
 
 #  ----------------------------------------------------------------------------
 #
